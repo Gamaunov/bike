02b4827942256c5088d6b58df317aa0e8620a87c
Denis <gamaunov1911@gmail.com>
2022-11-29 11:06:14 +0300

    slider JS

--- Mobi/app/js/main.js
+++ Mobi/app/js/main.js
@@ -1,15 +1,15 @@
-const anchors = document.querySelectorAll('a[href*="#"]');
+// const anchors = document.querySelectorAll('a[href*="#"]');
 
-for (let anchor of anchors) {
-  anchor.addEventListener("click", function (event) {
-    event.preventDefault();
-    const blockID = anchor.getAttribute("href");
-    document.querySelector("" + blockID).scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  });
-}
+// for (let anchor of anchors) {
+//   anchor.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     const blockID = anchor.getAttribute("href");
+//     document.querySelector("" + blockID).scrollIntoView({
+//       behavior: "smooth",
+//       block: "start",
+//     });
+//   });
+// }
 
 // $(function () {
 //   $(".slider").slick({
